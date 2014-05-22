82325db12937d4826574ae4577f7cbcaf1d9b8b0
malte-wessel <mail@breakdesign.de>
2014-05-22 15:32:52 +0200

    Haniel

---
+++ de/haniel.php
@@ -0,0 +1,59 @@
+<?php 
+
+return array(
+
+	'back' => 'Zurück zu Arbeiten',
+
+	'index' => array(
+		'meta' => array(
+			'title' => 'Haniel',
+			'description' => 'Migration der Website der Unternehmensgruppe in Familienbesitz mit Sitz in Duisburg Ruhrort',
+			'keywords' => 'TYPO3, SAP, Migration, Duisburg, Haniel',
+		),
+
+		'label' => 'Haniel',
+
+		'jumbotron' => array(
+			'headline1' => 'von SAP zu TYPO3',
+			'headline2' => 'Migration der Website der Unternehmensgruppe<br/> in Familienbesitz mit Sitz in Duisburg Ruhrort.',
+			'link1' => 'Projektvorstellung',
+			'link2' => 'Haniel.de'
+		)
+	),
+
+	'project' => array(
+		'meta' => array(
+			'title' => 'Projektvorstellung · Haniel',
+			'description' => 'Migration der Website der Unternehmensgruppe in Familienbesitz mit Sitz in Duisburg Ruhrort',
+			'keywords' => 'TYPO3, SAP, Migration, Duisburg, Haniel',
+		),
+
+		'label' => 'Projektvorstellung',
+
+		'jumbotron' => array(
+			'headline1' => 'Haniel',
+			'headline2' => 'Migration der Website',
+			'headline3' => 'von SAP zu TYPO3',
+			'headline4' => 'Haniel.de',
+		),
+
+		'section1' => array(
+			'headline' => 'Von SAP zu TYPO3',
+			'paragraph' => 'Mit der Migration von SAP zu TYPO3 erhielt HANIEL.de innerhalb kürzester Zeit eine zukunftssichere und skalierbare Heimat in dem enterprisefähigen Open-Source CMS TYPO3. Die Umsetzung erfolgte unter den Gesichtspunkten aktueller Anforderungen auf Basis responsiver Templates.'
+		),
+
+		'section2' => array(
+			'headline' => 'Vielseitig',
+			'paragraph' => 'Die vielseitigen Funktionen des CMS erlauben den Redakteuren des Systems bspw. das Anlegen von weiteren Websites (Multisite) und die Redaktion von Facebook-Tabs aus dem CMS heraus. So werden heute auch die Seiten der Haniel Stiftung und der Haniel Akademie zentral verwaltet.'
+		),
+
+		'features' => array(
+			'headline' => 'Besonderheiten',
+			'item1' => 'von SAP zu TYPO3',
+			'item2' => 'Multisite',
+			'item3' => 'Responsive Umsetzung',
+			'item4' => 'Facebook-Tabs',
+			'item5' => 'Mehrsprachigkeit'
+		)
+	)
+);
