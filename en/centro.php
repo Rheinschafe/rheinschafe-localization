908b2d77218834959850118504bb73f4a187136d
DDeflieze <DDeflieze@users.noreply.github.com>
2015-04-01 15:16:00 +0200

    Update centro.php

--- en/centro.php
+++ en/centro.php
@@ -2,72 +2,72 @@
 
 return array(
 
-	'back' => 'Zurück zu Arbeiten',
+	'back' => 'Back to cases',
 
 	'index' => array(
 
 		'meta' => array(
 			'title' => 'CentrO',
-			'description' => 'Europas größtes Shopping-und Freizeitzentrum',
-			'keywords' => 'Facebook, Apps, Fotowettbewerb, Centro',
+			'description' => 'Europe's biggest shopping- and leisure centre',
+			'keywords' => 'Facebook, Apps, photo contest, Centro',
 		),
 
 		'label' => 'CentrO',
 
 		'jumbotron' => array(
-			'headline1' => 'Europas größtes Shopping-und Freizeitzentrum',
+			'headline1' => 'Europe's biggest shopping- and leisure centre,
 			'link1' => 'Shopping Prince(ss)',
-			'link2' => 'Fotowettbewerb'
+			'link2' => 'Photo contest'
 		)
 	),
 
 	'shoppingprincess' => array(
 		'meta' => array(
 			'title' => 'Shopping Prince(ss) · CentrO',
-			'description' => 'Stylist und Designer Thomas Rath wählt aus',
-			'keywords' => 'Facebook, Apps, Fotowettbewerb, Centro'
+			'description' => 'Stylist and designer Thomas Rath selects the winner',
+			'keywords' => 'Facebook, Apps, photo contest, Centro'
 		),
 
 		'label' => 'Shopping Prince(ss)',
 
 		'jumbotron' => array(
 			'headline1' => 'Shopping Prince(ss)',
-			'headline2' => 'Stylist und Designer Thomas Rath wählt aus',
-			'link1' => 'Zur Aktion',
+			'headline2' => 'Stylist and designer Thomas Rath selects the winner',
+			'link1' => 'View contest',
 		),
 
 		'content' => array(
-			'headline' => 'Wettbewerb',
-			'paragraph' => 'Das Centro ermöglichte seinen Besuchern Fotos mit dem berühmten Stylist und Designer Thomas Rath zu machen. Der Designer war nicht persönlich anwesend, sondern wurde mit Hilfe eines Greensceens dem Foto hinzugefügt. Wer jedoch zu den besten 6 gewählt wurde, erhielt nicht nur ein Shoppingbudget von 750 Euro sondern auch die Möglichkeit bei einem erneuten Antreten gegen seine Mitstreiter den Designer persönlich zu treffen. Er selber wählte nämlich  den Gewinner einer Reise zur Fashionweek in Berlin aus.'
+			'headline' => 'Contest',
+			'paragraph' => 'The CentrO gave their visitors the opportunity to take a picture with the famous stylist and designer Thomas Rath. The designer wasn't there in person, but was added to the photo by means of a green screen. Still, those who got voted among the best six did not only win a shopping voucher valued at 750€, but also got the chance to meet the designer in the final. Thomas Rath himself selected the winner, who won a trip to the Fashionweek in Berlin.'
 		),
 
 		'features' => array(
-			'headline' => 'Besonderheiten',
-			'item1' => 'Responsive Umsetzung',
-			'item2' => 'One-Page-Design',
-			'item3' => 'Mitwirken des berühmten Thomas Rath',
-			'item4' => 'Greenscreen',
-			'item5' => 'Hauptgewinn: Besuch zur Fashionweek in Berlin'
+			'headline' => 'Features',
+			'item1' => 'Responsive design',
+			'item2' => 'One page design',
+			'item3' => 'Participation of celebrity Thomas Rath',
+			'item4' => 'Green screen',
+			'item5' => 'First prize: Trip to the Fashionweek in Berlin'
 		)
 	),
 
 	'fotowettbewerb' => array(
 		'meta' => array(
-			'title' => 'Fotowettbewerb · CentrO',
-			'description' => 'CentrO Oberhausen als Mode-Museum',
-			'keywords' => 'Facebook, Apps, Fotowettbewerb, Centro'
+			'title' => 'Photo contest · CentrO',
+			'description' => 'CentrO Oberhausen turned into a fashion museum',
+			'keywords' => 'Facebook, Apps, photo contest, Centro'
 		),
 
-		'label' => 'Fotowettbewerb',
+		'label' => 'Photo contest',
 
 		'jumbotron' => array(
 			'headline1' => 'Generation Fashion',
-			'headline2' => 'CentrO Oberhausen als Mode-Museum',
-			'headline3' => 'Zur Aktion'
+			'headline2' => 'CentrO Oberhausen turned into a fashion museum',
+			'headline3' => 'View contest'
 		),
 
 		'content' => array(
-			'paragraph1' => 'Beim Fotowettbewerb der Aktion „Generation Fashion“ des Centro Oberhausen wurden Modehighlights der letzten fünf Jahrzehnte präsentiert. Insegesamt 35 der Besten Teilnahmen konnten nicht nur im Weg sondern auch live im Centro bewundert werden. Hauptgewinn des Fotowettbewerb - eine Reise nach NewYork.'
+			'paragraph1' => 'The CentrO Oberhausen presented fashion highlights from the past five decades in a photo contest during the "Generation Fashion" event. The 35 best submissions could not only be admired online, but also live in the CentrO mall. The contest's first prize - a trip to New York.'
 		)
 	)
 
