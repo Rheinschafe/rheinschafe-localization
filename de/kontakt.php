7f74a96e19469cdc7b609f98651a6a44fcad6809
Malte W <wessel@rheinschafe.de>
2015-07-30 12:06:40 +0200

    privacy

--- de/kontakt.php
+++ de/kontakt.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 return array(
 
@@ -95,11 +95,23 @@ return array(
 			),
 			'privacy' => array(
 				'headline' => 'Datenschutz',
-				'paragraph' => 'In Verbindung mit dem Zugriff auf diese Webseite erfolgen zu Statistikzwecken Aufzeichnungen der Software Google Analytics. Informationen zum Google-Datenschutz <a href="http://www.google.com/intl/de_ALL/policies/privacy/" target="_blank" class="underline">finden Sie hier</a>.'
+				'paragraph' => 'In Verbindung mit dem Zugriff auf diese Webseite erfolgen zu Statistikzwecken Aufzeichnungen der Software Google Analytics. Informationen zum Google-Datenschutz <a href="http://www.google.com/intl/de_ALL/policies/privacy/" target="_blank" class="underline">finden Sie hier</a>.',
+				'paragraph2' => 'Klicken Sie hier für weitere Datenschutzhinweise oder wenn Sie Google Analytics deaktivieren wollen.'
 			),
 			'copyright' => array(
 				'headline' => 'Urheberrecht',
 				'paragraph' => 'Sämtliche Inhalte, Gestaltung und eventuelle Downloads, welche auf dieser Webseite zu finden sind, sind urheberrechtlich geschützt. Die kommerzielle Vervielfältigung und Verarbeitung der eingestellten Inhalte benötigen eine vorherige, schriftliche Zustimmung. Die nichtkommerzielle Verwendung von Artikeln und Zitaten bedarf bei Beachtung dieser <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/deed.de" target="_blank" class="underline">Creative Commons Lizenz</a> keiner weiteren Zustimmung.'
+			)
+		),
+
+		'modals' => array(
+			'privacy' => array(
+				'headline' => 'Datenschutzhinweise',
+				'paragraph1' => 'Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“). Google Analytics verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert.',
+				'paragraph2' => 'Im Falle der Aktivierung der IP-Anonymisierung auf dieser Webseite, wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Die IP-Anonymisierung ist auf dieser Website aktiv. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen.',
+				'paragraph3' => 'Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt. Sie können die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website vollumfänglich werden nutzen können. Sie können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem sie das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren: <a href="http://tools.google.com/dlpage/gaoptout?hl=de">http://tools.google.com/dlpage/gaoptout?hl=de</a>.',
+				'paragraph4' => 'Alternativ zum Browser-Add-On oder innerhalb von Browsern auf mobilen Geräten, klicken Sie bitte unten auf „Google Analytics deaktivieren“, um die Erfassung durch Google Analytics innerhalb dieser Website zukünftig zu verhindern (das Opt Out funktioniert nur in dem Browser und nur für diese Domain). Dabei wird ein Opt-Out-Cookie auf Ihrem Ger&auml;t abgelegt. L&ouml;schen Sie Ihre Cookies in diesem Browser, m&uuml;ssen Sie diesen Link erneut klicken.',
+				'button' => 'Google Analytics deaktivieren'
 			)
 		)
 	),
